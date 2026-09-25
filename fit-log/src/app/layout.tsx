e1223f8e366f968b6c78a385b5439c6c89108aa4
itmum <itmamsadiq22@gmail.com>
2026-09-26 04:42:50 +0600

    fix: explicit dark theme configuration inside tailwind v4 css

--- fit-log/src/app/layout.tsx
+++ fit-log/src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="dark"
     >
       <body className="min-h-full flex flex-col">
         <ToastContainer></ToastContainer>
